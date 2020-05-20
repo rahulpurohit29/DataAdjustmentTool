6898a7cf258c0460726ad70e51f75ee3aba90b83
neha6767j <63315339+neha6767j@users.noreply.github.com>
2020-05-20 12:27:49 +0530

    Update views.py

--- django/dataadjustmenttool/views.py
+++ django/dataadjustmenttool/views.py
@@ -1,6 +1,8 @@
+from django.views.generic import View
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Student_details
+from dataadjustmenttool import models
+
 
 def upload_csv(request):
 	data = {}
@@ -9,21 +11,17 @@ def upload_csv(request):
     # if not GET, then proceed
 	try:
 		csv_file = request.FILES["csv_file"]
-		if not csv_file.name.endswith('.csv'):
-			messages.error(request,'File is not CSV type')
-			return HttpResponseRedirect(reverse("myapp:upload_csv"))
-        
 		file_data = csv_file.read().decode("utf-8")		
 
 		lines = file_data.split("\n")
 		#loop over the lines and save them in db. If error , store as string and then display
 		for line in lines:						
 			fields = line.split(",")
-			data_dict = {}
-			data_dict["name"] = fields[0]
-			data_dict["start_date_time"] = fields[1]
-			data_dict["end_date_time"] = fields[2]
-			data_dict["notes"] = fields[3]
+			data_dict = {"SID": fields[0], "FIRST_NAME": fields[1], "MIDDLE_NAME": fields[2], "LAST_NAME": fields[3],
+                     "VLD_FROM": fields[4], "VLD_TILL": fields[5]
+                     }
+
+			print(data_dict)
 			try:
 				form = EventsForm(data_dict)
 				if form.is_valid():
@@ -38,4 +36,8 @@ def upload_csv(request):
 		logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
 		messages.error(request,"Unable to upload file. "+repr(e))
 
-	return HttpResponseRedirect(reverse("myapp:upload_csv"))
+	return HttpResponseRedirect(reverse("upload_csv"))
+'''
+class FrontendRenderView(View):
+        def get(self, request, *args, **kwargs):
+                return render(request, "angular/src/app/apps.component.html", {}) '''
