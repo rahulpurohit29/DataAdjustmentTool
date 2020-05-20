b6d771370e57947b9165fa564f177d172a1f9fa7
neha6767j <63315339+neha6767j@users.noreply.github.com>
2020-05-20 08:56:31 +0530

    Update views.py

--- django/dataadjustmenttool/views.py
+++ django/dataadjustmenttool/views.py
@@ -2,42 +2,40 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from models import Student_details
 
-
-@permission_required('admin.can_add_log_entry')
 def upload_csv(request):
-    template = "app.component.html"
-    prompt = {
-        'order' : 'Order of csv should be SID, FIRST_NAME. MIDDLE_NAME, LAST_NAME, VLD_FROM, VLD_TILL'
-    }
-    if request.method == 'GET':
-        return render(request, template, prompt)
+	data = {}
+	if "GET" == request.method:
+		return render(request, "myapp/upload_csv.html", data)
+    # if not GET, then proceed
+	try:
+		csv_file = request.FILES["csv_file"]
+		if not csv_file.name.endswith('.csv'):
+			messages.error(request,'File is not CSV type')
+			return HttpResponseRedirect(reverse("myapp:upload_csv"))
+        
+		file_data = csv_file.read().decode("utf-8")		
 
-    csv_file = request.FILES['change-input']
+		lines = file_data.split("\n")
+		#loop over the lines and save them in db. If error , store as string and then display
+		for line in lines:						
+			fields = line.split(",")
+			data_dict = {}
+			data_dict["name"] = fields[0]
+			data_dict["start_date_time"] = fields[1]
+			data_dict["end_date_time"] = fields[2]
+			data_dict["notes"] = fields[3]
+			try:
+				form = EventsForm(data_dict)
+				if form.is_valid():
+					form.save()					
+				else:
+					logging.getLogger("error_logger").error(form.errors.as_json())												
+			except Exception as e:
+				logging.getLogger("error_logger").error(repr(e))					
+				pass
 
-    if not csv_file.name.endswith('.csv'):
-        messages.error(request, 'This is not a csv file.')
+	except Exception as e:
+		logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
+		messages.error(request,"Unable to upload file. "+repr(e))
 
-    data_set = csv_file.read().decode("utf-8")
-
-
-    lines = data_set.split("\n")
-    for line in lines:
-        fields = line.split(",")
-        data_dict = {"SID": fields[0], "FIRST_NAME": fields[1], "MIDDLE_NAME": fields[2], "LAST_NAME": fields[3],
-                     "VLD_FROM": fields[4], "VLD_TILL": fields[5]
-                     }
-        try:
-            form = EventsForm(data_dict)
-            if form.is_valid():
-                form.save()
-            else:
-                logging.getLogger("error_logger").error(form.errors.as_json())
-        except Exception as e:
-            logging.getLogger("error_logger").error(repr(e))
-            pass
-    except Exception as e:
-    logging.getLogger("error_logger").error("Unable to upload file. " + repr(e))
-    messages.error(request, "Unable to upload file. " + repr(e))
-    context = {}
-    render(request, template, context)
-
+	return HttpResponseRedirect(reverse("myapp:upload_csv"))
