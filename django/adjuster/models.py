36e03515b29636e75b0753f85bd4c33669a7cde5
kvpankaj1999 <kvpankaj1999@gmail.com>
2020-05-25 11:37:57 +0530

    added the latest data file and changed the models.py file

--- django/adjuster/models.py
+++ django/adjuster/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Student_details(models.Model):
-    stud_id = models.CharField(max_length = 50)
+    stud_id = models.CharField(max_length = 50,primary_key=True)
     first_name = models.CharField(max_length = 50)
     middle_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     valid_from = models.DateField(auto_now=False, auto_now_add=True)
     valid_to = models.DateField(auto_now=True, auto_now_add=False)
+    latest= models.BooleanField()
