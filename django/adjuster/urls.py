a91b1c484de0e59c24af31753174d6477c164d4c
neha6767j <63315339+neha6767j@users.noreply.github.com>
2020-05-25 11:49:16 +0530

    Update urls.py

--- django/adjuster/urls.py
+++ django/adjuster/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url,include
+from django.urls import path
 from adjuster import views
-from views import upload_csv
+from views import upload_csv, download_csv
 
 urlpatterns = [
     url(r'^adjuster',include('adjuster.urls')),
+    path('upload_csv', views.upload_csv),
+    path('export/csv-database-write/', views.csv_database_write, name='csv_database_write'),
 ]
