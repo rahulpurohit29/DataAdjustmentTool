dca1d4621d4cd835862633ca944c8320518424ec
rahulpurohit29 <rrp291999@gmail.com>
2020-05-28 10:26:14 +0530

    Added ANgular Test Framework

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
@@ -19,17 +25,14 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'DataAdjustmentTool'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('DataAdjustmentTool');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('DataAdjustmentTool app is running!');
-  });
+  it(`should have text 'DATA ADJUSTMENT TOOL'`, ()=>{
+    const HeadingElement=<HTMLHeadingElement>document.getElementById('heading');
+    expect(HeadingElement.textContent).toBe("DATA ADJUSTMENT TOOL");
+  }
+  );
+  it(`should have label 'CSV update'`, ()=>{
+    const LabelElement=<HTMLLabelElement>document.getElementById("label");
+    expect(LabelElement.innerText).toBe("CSV update");
+  }
+  );
 });
